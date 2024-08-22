2c6a7df148cb1572824adc90073268de71f559d5
Rohini <rohisk482@gmail.com>
2024-08-22 20:50:10 +0530

    first commit

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "./Login.css";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/login-success");
+  };
+
+  return (
+    <div className="login-form-container">
+      <form onSubmit={handleSubmit} className="login-form">
+        <h2 className="login-form-title">SignIn</h2>
+
+        <div className="input-group">
+          <label htmlFor="email" className="login-label">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="login-input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="password" className="login-label">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="login-input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        <button type="submit" className="login-button">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/Components/LoginSuccess.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginSuccess = () => {
+  return <div>LoginSuccessul</div>;
+};
+
+export default LoginSuccess;
